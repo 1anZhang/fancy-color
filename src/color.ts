0be8cd97ea9c7d23be4eb76144f21182b9b27297
ZhangYxY <512224856@qq.com>
2019-12-02 15:20:38 +0800

    :sparkles: add mix color function

--- src/color.ts
+++ src/color.ts
@@ -71,7 +71,7 @@ class Color {
     }
   }
 
-  toHexString(allow3Char = false) {
+  toHexString(allow3Char = false): string {
     let r = decNumberToHexString(this._r);
     let g = decNumberToHexString(this._g);
     let b = decNumberToHexString(this._b);
@@ -85,15 +85,15 @@ class Color {
     return `#${r}${g}${b}`;
   }
 
-  toRgbString() {
+  toRgbString(): string {
     return `rgb(${this._r}, ${this._g}, ${this._b}`;
   }
 
-  toRgbaString() {
+  toRgbaString(): string {
     return `rgba(${this._r}, ${this._g}, ${this._b}, ${this._a})`;
   }
 
-  toHslString(precision = 0) {
+  toHslString(precision = 0): string {
     const rgb: IRgbColor = {
       r: this._r,
       g: this._g,
@@ -103,7 +103,7 @@ class Color {
     return `hsl(${hsl.h}, ${hsl.s}%, ${hsl.l}%)`;
   }
 
-  toHslaString(precision = 0) {
+  toHslaString(precision = 0): string {
     const rgb: IRgbColor = {
       r: this._r,
       g: this._g,
@@ -112,6 +112,67 @@ class Color {
     const hsl = rgbToHsl(rgb, precision);
     return `hsla(${hsl.h}, ${hsl.s}%, ${hsl.l}%, ${this._a})`;
   }
+
+  getRgbObject(): IRgbColor {
+    return {
+      r: this._r,
+      g: this._g,
+      b: this._b
+    }
+  }
+
+  tint(percentage: number) {
+    return Color.tint(this.getRgbObject(), percentage);
+  }
+
+  shade(percentage: number) {
+    return Color.shade(this.getRgbObject(), percentage);
+  }
+
+  mix(targetColor: IRgbColor, percentage: number) {
+    return Color.mix(this.getRgbObject(), targetColor, percentage);
+  }
+
+  getHoverColor(): string {
+    return this.tint(80).toHexString();
+  }
+
+  getActiveColor(): string {
+    return this.shade(5).toHexString();
+  }
+
+  getColorGradeList(): Array<string> {
+    return [
+      this.tint(20).toHexString(),
+      this.tint(40).toHexString(),
+      this.tint(60).toHexString(),
+      this.tint(80).toHexString(),
+      this.toHexString(),
+      this.shade(20).toHexString(),
+      this.shade(40).toHexString(),
+      this.shade(60).toHexString(),
+      this.shade(80).toHexString(),
+    ];
+  }
+
+  static tint(color: IRgbColor, percentage: number) {
+    return Color.mix(new Color('#fff').getRgbObject(), color, percentage);
+  }
+
+  static shade(color: IRgbColor, percentage: number) {
+    return Color.mix(color, new Color('#000').getRgbObject(), percentage);
+  }
+
+  static mix(originColor: IRgbColor, targetColor: IRgbColor, percentage: number) {
+    let r = (targetColor.r - originColor.r) * (percentage / 100);
+    let g = (targetColor.g - originColor.g) * (percentage / 100);
+    let b = (targetColor.b - originColor.b) * (percentage / 100);
+    r = Math.floor(r) + originColor.r;
+    g = Math.floor(g) + originColor.g;
+    b = Math.floor(b) + originColor.b;
+    return new Color({ r, g, b });
+  }
+
 }
 
 export default Color;
